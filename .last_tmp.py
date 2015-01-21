ad11476253a1c49479cc8d238597d8ca83bc027b
Afwan Wali Hamim <afwanwalihamim@gmail.com>
2015-01-21 16:29:55 +0700

    try

---
+++ .last_tmp.py
@@ -0,0 +1,12 @@
+#this apps is open
+
+#Let's start with introduction
+
+print "Hi, I am x0x. Could we introduce ourselves? (yes/no)\n"
+answer = input()
+if answer.lower() == 'yes':
+    print "Okay, what is your name"
+elif answer.lower() == 'no':
+    print "thank you"
+else:
+    print "your answer is wrong"
